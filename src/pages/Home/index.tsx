2332587f4809a3d624f8a45aa8a23fd6d2ffb17c
Dev-JeanSantos <jeancbsan@gmail.com>
2023-07-14 15:13:01 -0300

    implementacao e estilizando a Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import "./styles.scss";
 
 const Home = () => (
-<div>
-    <h1>HOME</h1>
-</div>
+    <div>
+        <div className='home-container'>
+        <div className="row home-content card-base border-radius-20">
+            <h1>HOME</h1>
+        </div>
+        </div>
+    </div>
 );
 
 export default Home;
