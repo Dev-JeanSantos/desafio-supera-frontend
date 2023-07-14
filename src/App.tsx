f3fefaf9a092352214b081246dbb3ae0f14c8c5f
Dev-JeanSantos <jeancbsan@gmail.com>
2023-07-14 14:38:05 -0300

    implementando rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
+import Routes from './Routes';
 import './app.scss';
 import './core/assets/styles/custom.scss';
-import Navbar from './core/components/Navbar';
 
 const App = () =>{
     return(
-        <Navbar />
+        <Routes />
     );
 }
 
--- src/core/components/Navbar/index.tsx
+++ src/core/components/Navbar/index.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
 import "./styles.css";
 
 const Navbar = () => (
     <nav className='row bg-primary main-nav'>
         <div className='col-4 '>
-            <h4 className='main-text'>DESAFIO FRONTEND SUPERA</h4>
+            <Link to='/' className='nav-logo-text'>
+                <h4 className='main-text'>DESAFIO FRONTEND SUPERA</h4>
+            </Link>
         </div>
     </nav>
 )
